bd679dadad6d0ecaa470dea01c164f6c0ffae899
Nikola Jovanovski <nikolajovanovski234@gmail.com>
2025-06-10 21:44:19 +0200

    Implement basic file copying in chunks using FileStream

--- FileTransferToolTask/Program.cs
+++ FileTransferToolTask/Program.cs
@@ -3,13 +3,40 @@
     {
         public static void CopyFile(string sourcePath, string destinationPath)
         {
-            // TODO: Implement file copying
+            if (!File.Exists(sourcePath))
+            {
+                Console.WriteLine($"Source does not exist at '{sourcePath}'.");
+                return;
+            }
+
+            string fileName = Path.GetFileName(sourcePath);
+            destinationPath = Path.Combine(destinationPath, fileName);
+
+            const int ChunkSize = 1024 * 1024;
+            using FileStream fsRead = new FileStream(sourcePath, FileMode.Open, FileAccess.Read);
+            using FileStream fsWrite = new FileStream(destinationPath, FileMode.Create, FileAccess.ReadWrite);
+
+            byte[] buffer = new byte[ChunkSize];
+            while (true)
+            {
+                int bytesRead = fsRead.Read(buffer, 0, ChunkSize);
+                if (bytesRead == 0)
+                    break;
+
+                fsWrite.Write(buffer, 0, bytesRead);
+            }
+            Console.WriteLine("File copied successfully.");
 
         }
         public static void Main(string[] args)
         {
-            string source = "";
-            string destination = "";
+            if (args.Length < 2)
+            {
+                Console.WriteLine("Usage: FileTransferTool <sourcePath> <destinationPath>");
+                return;
+            }
+            string source = args[0];
+            string destination = args[1];
             CopyFile(source, destination);
         }
     }
