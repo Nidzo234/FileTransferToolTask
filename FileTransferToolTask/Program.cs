167a1c9bc443bb72b229fc891cbfd5942367345f
Nikola Jovanovski <nikolajovanovski234@gmail.com>
2025-06-10 21:21:09 +0200

    Add project files.

---
+++ FileTransferToolTask/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace FileTransferToolTask {
+    class Program
+    {
+        public static void CopyFile(string sourcePath, string destinationPath)
+        {
+            // TODO: Implement file copying
+
+        }
+        public static void Main(string[] args)
+        {
+            string source = "";
+            string destination = "";
+            CopyFile(source, destination);
+        }
+    }
+}
