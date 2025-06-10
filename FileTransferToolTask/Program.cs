7181c14f80ede800d7e7c34fc8761f50e56ad056
Nikola Jovanovski <nikolajovanovski234@gmail.com>
2025-06-10 22:19:11 +0200

    Add MD5 chunk hashing and validation in copy process

--- FileTransferToolTask/Program.cs
+++ FileTransferToolTask/Program.cs
@@ -1,4 +1,6 @@
-﻿namespace FileTransferToolTask {
+﻿using System.Security.Cryptography;
+
+namespace FileTransferToolTask {
     class Program
     {
         public static void CopyFile(string sourcePath, string destinationPath)
@@ -8,22 +10,39 @@
                 Console.WriteLine($"Source does not exist at '{sourcePath}'.");
                 return;
             }
+            Console.WriteLine("Starting copying file...");
 
             string fileName = Path.GetFileName(sourcePath);
             destinationPath = Path.Combine(destinationPath, fileName);
 
             const int ChunkSize = 1024 * 1024;
+            byte[] buffer = new byte[ChunkSize];
+            int totalBytesCopied = 0;
+
             using FileStream fsRead = new FileStream(sourcePath, FileMode.Open, FileAccess.Read);
             using FileStream fsWrite = new FileStream(destinationPath, FileMode.Create, FileAccess.ReadWrite);
 
-            byte[] buffer = new byte[ChunkSize];
             while (true)
             {
                 int bytesRead = fsRead.Read(buffer, 0, ChunkSize);
                 if (bytesRead == 0)
                     break;
 
-                fsWrite.Write(buffer, 0, bytesRead);
+                byte[] sourceChunk = buffer[..bytesRead];
+                byte[] sourceHash = MD5.HashData(sourceChunk);
+
+                fsWrite.Write(sourceChunk, 0, bytesRead);
+                fsWrite.Seek(totalBytesCopied, SeekOrigin.Begin);
+
+                byte[] destinationChunk = new byte[bytesRead];
+                fsWrite.Read(destinationChunk, 0, bytesRead);
+                byte[] destinationHash = MD5.HashData(destinationChunk);
+
+                if (!sourceHash.SequenceEqual(destinationHash))
+                {
+                    Console.WriteLine("Chunk failed verification!!!");
+                }
+                totalBytesCopied += bytesRead;
             }
             Console.WriteLine("File copied successfully.");
 
